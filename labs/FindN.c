5f4e06128a9372fb95c52eacad9d5bd8667645a9
EC2 Default User <ec2-user@ip-172-31-41-237.us-east-2.compute.internal>
2020-01-15 19:42:22 +0000

    Find the exact n0 for Q7, lab1

---
+++ labs/FindN.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+double logB(double x, double base);
+
+main()
+{
+	int i;
+	double x = 100*log
+
+	for(i=1; ; ++i)
+}
+
+double logB(double x, double base) { return log(x)/log(base); }
+
