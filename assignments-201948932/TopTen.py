bb24a094ba91552085ea4fd6fe93ab3c81abbc38
EC2 Default User <ec2-user@ip-172-31-41-237.us-east-2.compute.internal>
2020-01-22 03:53:41 +0000

    make python base code of picking top ten integers for assignment1

---
+++ assignments-201948932/TopTen.py
@@ -0,0 +1,20 @@
+##
+##  TopTen.py
+##  Assignment-201948932
+##  
+##  Created by Hong Guen Ji on 18/01/20
+##  VIM - Vi IMproved 8.0
+##  Copyright © 2020 Hong Guen Ji. All rights reserved.
+##
+##  Returning top ten integers from given list
+##  
+
+def returnTopTenList(lst):
+    # find 10 largest integers from the list
+    pass
+    
+
+if __name__ == "__main__":
+    lst = []
+    returnTopTenList(lst)
+    
