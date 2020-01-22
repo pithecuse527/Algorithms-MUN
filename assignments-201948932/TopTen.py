8ba5c400e0abb2bd64a23ebd78528fe36e072b08
EC2 Default User <ec2-user@ip-172-31-41-237.us-east-2.compute.internal>
2020-01-22 04:31:03 +0000

    for sorting first, add bubblesort

--- assignments-201948932/TopTen.py
+++ assignments-201948932/TopTen.py
@@ -9,6 +9,17 @@
 ##  Returning top ten integers from given list
 ##  
 
+
+def bubbleSort(lst):
+    # basic bubble sort
+    # time complexity -> O(n^2)
+    
+    for i in range(len(lst) - 1):
+        for j in range(1, len(lst) - i):
+            if lst[j - 1] > lst[j]:
+                lst[j - 1], lst[j] = lst[j], lst[j - 1]
+
+
 def returnTopTenList(lst):
     # find 10 largest integers from the list
     pass
