5356b028ba125edb583f8928adcb834376c01871
EC2 Default User <ec2-user@ip-172-31-41-237.us-east-2.compute.internal>
2020-01-22 05:22:58 +0000

    create basic codes for harmonic number recursive algorithms

---
+++ assignments-201948932/HarmonicNum.py
@@ -0,0 +1,21 @@
+##
+##  HarmonicNum.py
+##  Assignment-201948932
+##  
+##  Created by Hong Guen Ji on 18/01/20
+##  VIM - Vi IMproved 8.0
+##  Copyright © 2020 Hong Guen Ji. All rights reserved.
+##
+##  Calculate harmonic number by recursive algorithms
+##  
+
+#!/usr/bin/python
+
+def harmonic():
+    
+    pass
+
+if __name__ == "__main__":
+    n = int(input("num? "))
+    
+    harmonic_num = harmonic(n)
