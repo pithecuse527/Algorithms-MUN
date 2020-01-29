29e2bb4dc6718268192b10a0884b3455a4ee71ee
EC2 Default User <ec2-user@ip-172-31-41-237.us-east-2.compute.internal>
2020-01-29 17:29:02 +0000

    initial commit for spans2 algorithms

---
+++ labs/Spans.py
@@ -0,0 +1,23 @@
+##
+##  Spans.py
+##  Assignment-201948932
+##  
+##  Created by Hong Guen Ji on 29/01/20
+##  VIM - Vi IMproved 8.0
+##  Copyright © 2020 Hong Guen Ji. All rights reserved.
+##
+##  Implement the algorithm spans2 from COMP2002 materials
+##  
+
+#!/usr/bin/python
+
+import random
+
+def spans(data_lst):
+    pass
+
+
+if __name__ == "__main__":
+    
+    data_lst = [6,3,4,2,5,1,10]
+        
