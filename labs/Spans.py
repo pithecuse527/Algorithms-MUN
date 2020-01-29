a89be79d24c56a274036425fb79f260afbfb4596
EC2 Default User <ec2-user@ip-172-31-41-237.us-east-2.compute.internal>
2020-01-29 18:44:43 +0000

    complete the spans2 algorithm

--- labs/Spans.py
+++ labs/Spans.py
@@ -12,12 +12,36 @@
 #!/usr/bin/python
 
 import random
+import MyStack
 
 def spans(data_lst):
-    pass
+    # spans2 algorithms implementation
+    
+    A = MyStack.Stack()     # new empty tmp stack
+    S = [None] * 20                  # new array of n integers
+    # n = len(A.items)
+    
+    for i in range(len(data_lst)):
+        n = len(A.items)            # needs to be keep tracked
+        while n and (data_lst[A.peek()] <= data_lst[i]):
+            A.pop()
+            n = len(A.items)            # needs to be keep tracked
+        
+        if A.isEmpty():
+            S[i] = i + 1
+        else:
+            S[i] = i - A.peek()
+        
+        A.push(i)
 
+    return S
 
 if __name__ == "__main__":
     
     data_lst = [6,3,4,2,5,1,10]
-        
+    #data_lst 
+    #data_lst.items = [6,3,4,2,5,1,10]       # just for the test
+    
+    #spans(data_lst)
+    print(spans(data_lst))
+    
