7ed91b368f50cb4cafd73e6ae566411948030e88
EC2 Default User <ec2-user@ip-172-31-41-237.us-east-2.compute.internal>
2020-02-03 11:38:53 +0000

    implement basic stack class

--- labs/MyStack2.py
+++ labs/MyStack2.py
@@ -12,8 +12,43 @@
 #!/usr/bin/python
 
 class ArrayStack:
-    """LIFO Stack implementation using a Python list as underlying storage."""
+    """LIFO Stack implementation using a Python list as underlying storage"""
     
     def __init__(self):
         """Create an empty stack"""
-        self.data = []
+        self._data = []
+
+    def __len__(self):
+        """Return the number of elements in the stack"""
+        return len(self._data)
+    
+    def is_empty(self):
+        """Return True if the stack is empty"""
+        return len(self._data) == 0
+    
+    def push(self, e):
+        """Add element e to the top of the stack"""
+        self._data.append(e)        # new item stored at end of list
+    
+    def top(self):
+        """Return (but do not remove) the element at the top of the stack
+        Raise Empty exception if the stack is empty"""
+        try:
+            t = self._data[-1]
+        except:
+            print("Stack is empty!")
+            # raise
+        else:
+            return t
+    
+    def pop(self):
+        """Remove and return the element from the top of the stack (i.e. LIFO)
+        Raise Empty exception if the stack is empty"""
+        
+        try:
+            p = self._data.pop()
+        except:
+            print("Stack is empty!")
+            # raise
+        else:
+            return p
