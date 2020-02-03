8d62b31bb4824776acae53cc7bae8ac3796e29c2
EC2 Default User <ec2-user@ip-172-31-41-237.us-east-2.compute.internal>
2020-02-03 12:22:00 +0000

    initial commit for basic queue implement

---
+++ labs/Queue.py
@@ -0,0 +1,31 @@
+##
+##  Queue.py
+##  labs-201948932
+##  
+##  Created by Hong Guen Ji on 03/02/20
+##  VIM - Vi IMproved 8.0
+##  Copyright © 2020 Hong Guen Ji. All rights reserved.
+##
+##  Basic queue calss (Implementation from COMP2002 stack ADT)
+##
+
+#!/usr/bin/python
+
+class ArrayQueue:
+    """FIFO queue implementation using a Python list as underlying storage"""
+    DEFAULT_CAPACITY = 10
+    
+    def __init__(self):
+        """Create an empty queue"""
+        self._data = [None] * ArrayQueue.DEFAULT_CAPACITY
+        self._size = 0
+        self._front = 0
+    
+    def __len__(self):
+        """Return the number of elements in the scope"""
+        return self._size
+        
+    def is_empty(self):
+        """Return True if the queue is empty"""
+        return self._size == 0
+    
