cda25500e16c285610ec8d065e828a2f076229d3
EC2 Default User <ec2-user@ip-172-31-41-237.us-east-2.compute.internal>
2020-02-05 01:20:42 +0000

    complete methods for the circular queue

--- labs/Queue.py
+++ labs/Queue.py
@@ -18,8 +18,8 @@ class ArrayQueue:
     def __init__(self):
         """Create an empty queue"""
         self._data = [None] * ArrayQueue.DEFAULT_CAPACITY
-        self._size = 0
-        self._front = 0
+        self._size = 0      # the actual size of the queue
+        self._front = 0     # the front of the queue
     
     def __len__(self):
         """Return the number of elements in the scope"""
@@ -32,7 +32,6 @@ class ArrayQueue:
     def first(self):
         """Return (but do not remove) the element at the front of the queue
         Raise Empty exception if the queue is empty"""
-        
         try:
             f = self._data[self._front]
         except:
@@ -43,5 +42,44 @@ class ArrayQueue:
         else:
             return f
     
+    def dequeue(self):
+        """Remove and return the first element of the queue (i.e. FIFO)
+        Raise Empty exception if the queue is empty"""
+        try:
+            d = self._data[self._front]
+        except:
+            if self.is_empty():
+                print("Queue is empty!")
+            else:
+                print("There is a problem with your queue...")
+        else:
+            self._data[self._front] = None
+            self._front = (self._front + 1) % ArrayQueue.DEFAULT_CAPACITY
+            self._size -= 1
+            return d
             
-    
+    def enqueue(self, e):
+        """Add an element to the back of queue"""
+        try:
+            self._data[self._front + self._size] = e            # try the normal enqueue process
+        except IndexError:
+            if self._size == len(self._data):                   # if the queue needs more space, double it
+                self._resize(2 * ArrayQueue.DEFAULT_CAPACITY)
+            self._data[(self._front + self._size) % len(self._data)] = e    # try the circular queue enqueue process
+                                                                            # this will implement anyway whether the queue is expanded or not
+        except:
+            raise("There is a problem with your queue...")
+        self._size += 1
+            
+    def _resize(self, cap):                 # assume cap >= len(self)
+        """Resize to a new list of capacity >- len(self)
+        And realined the elements to a new list"""
+        old = self._data                    # keep track of existing list
+        self._data = [None] * cap           # allocate list with new capacity
+        walk = self._front
+
+        for k in range(self._size):         # only consider existing elements
+            self._data[k] = old[walk]       # intentionally shift indices
+            walk = (1 + walk) % len(old)    # use old size as modulus
+        self._front = 0                     # front has been realigned
+        
