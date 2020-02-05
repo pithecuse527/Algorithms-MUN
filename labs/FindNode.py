6873192e6ea6b1c307f97b407bb6a0400ed16d0a
EC2 Default User <ec2-user@ip-172-31-41-237.us-east-2.compute.internal>
2020-02-05 01:22:42 +0000

    initial commit for node finding implementation

---
+++ labs/FindNode.py
@@ -0,0 +1,12 @@
+##
+##  FindNode.py
+##  labs-201948932
+##  
+##  Created by Hong Guen Ji on 04/02/20
+##  VIM - Vi IMproved 8.0
+##  Copyright © 2020 Hong Guen Ji. All rights reserved.
+##
+##  Finding second-to-last node in a singly linked list
+##
+
+#!/usr/bin/python
