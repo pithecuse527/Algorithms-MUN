ae6d04a8fba59d31fd5f21d5c02c9a27efed13bc
EC2 Default User <ec2-user@ip-172-31-41-237.us-east-2.compute.internal>
2020-02-05 01:23:06 +0000

    initial commit for the stack with the linked list

---
+++ labs/LinkedStack.py
@@ -0,0 +1,15 @@
+##
+##  LinkedStack.py
+##  labs-201948932
+##  
+##  Created by Hong Guen Ji on 04/02/20
+##  VIM - Vi IMproved 8.0
+##  Copyright © 2020 Hong Guen Ji. All rights reserved.
+##
+##  Basic stack implementation by using linked list
+##
+
+#!/usr/bin/python
+
+class LinkedStack:
+    pass
