604efb73eaafde144b473b96d2db39134b85ed43
EC2 Default User <ec2-user@ip-172-31-41-237.us-east-2.compute.internal>
2020-02-05 02:27:33 +0000

    complete the basic method for linked-list stack

--- labs/LinkedStack.py
+++ labs/LinkedStack.py
@@ -12,4 +12,62 @@
 #!/usr/bin/python
 
 class LinkedStack:
-    pass
+    """LIFO Stack implementation using a singly linked list for storage"""
+    
+    #-------------------- nested _Node class -------------------- #
+    
+    class _Node:
+        """Lightweight, nonpublic class for storing a singly linked node"""
+        __slots__ = '_element', '_next'     # streamline memory usage
+        
+        def __init__(self, element, next):  # initialize node's fields
+            self._element = element         # reference to user's element
+            self._next = next               # reference to next node
+            
+    #-------------------- nested _Node class -------------------- #
+    
+    def __init__(self):
+        """Create an empty stack."""
+        self._head = None                   # reference to the head node
+        self._size = 0                      # reference of stack elements
+        
+    def __len__(self):
+        """Return the number of elements in the stack"""
+        return self._size
+    
+    def is_empty(self):
+        """Return True if the stack is empty"""
+        return self._size == 0
+    
+    def push(self, e):
+        """Add element e to the top of the stack"""
+        self._head = self._Node(e, self._head)
+        self._size += 1
+    
+    def top(self):
+        """Return (but do not remove) the element at the top of the stack
+        Raise Empty exception if the stack is empty"""
+        try:
+            t = self._head._element
+        except:
+            if self.is_empty():
+                print("Stack is empty!")
+            else:
+                print("There is a problem with your stack...")
+        else:
+            return t
+    
+    def pop(self):
+        """Remove and return the element from the top of the stack (i.e. LIFO)
+        Raise Empty exception if the stack is empty"""
+        try:
+            p = self._head._element
+        except:
+            if self.is_empty():
+                print("Stack is empty!")
+            else:
+                print("There is a problem with your stack...")
+        else:
+            self._head = self._head._next
+            self._size -= 1
+            return p
