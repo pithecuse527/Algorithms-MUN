39655be25a06b75a516df4412bc2f19fa72ac5dd
EC2 Default User <ec2-user@ip-172-31-41-237.us-east-2.compute.internal>
2020-02-09 01:41:23 +0000

    initial commit for Postfix.py

---
+++ assignments-201948932/a2/Postfix.py
@@ -0,0 +1,24 @@
+##
+##  Postfix.py
+##  Assignment-201948932
+##  
+##  Created by Hong Guen Ji on 08/02/20
+##  VIM - Vi IMproved 8.0
+##  Copyright © 2020 Hong Guen Ji. All rights reserved.
+##
+##  Calculating postfix notation
+##  
+
+#!/usr/bin/python
+
+import array_stack
+
+def isPostfix():
+    pass
+
+if __name__ == "__main__":
+    
+    p_notatioin = input("Type only the postfix notation : ")
+    
+    # while(not isPostfix(p_notatioin)):
+    
