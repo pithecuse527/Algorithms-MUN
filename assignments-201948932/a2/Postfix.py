90374d9c3385507c6d68ca8c1238d56b881655e1
EC2 Default User <ec2-user@ip-172-31-41-237.us-east-2.compute.internal>
2020-02-09 02:55:14 +0000

    add calculating postfix notation function

--- assignments-201948932/a2/Postfix.py
+++ assignments-201948932/a2/Postfix.py
@@ -13,12 +13,70 @@
 
 import array_stack
 
-def isPostfix():
-    pass
+def calculatePostfix(formula):
+    stk = array_stack.ArrayStack()
+    
+    p_notation = formula.split(" ")     # split elements by the space
+    
+    # walk through the formula and calculate
+    # if the user did not input a postfix notation, raise
+    for walker in p_notation:
+        # if the walker meets operator, pop two element and push the calculation result
+        
+        if walker == '+':
+            tmp = stk.pop()
+            try:
+                stk.push(stk.pop() + tmp)
+            except:
+                print("your formula seems not a postfix notation")
+                return None
+            
+        elif walker == '-':
+            tmp = stk.pop()
+            try:
+                stk.push(stk.pop() - tmp)
+            except:
+                print("your formula seems not a postfix notation")
+                return None
+            
+        elif walker == '*':
+            tmp = stk.pop()
+            try:
+                stk.push(stk.pop() * tmp)
+            except:
+                print("your formula seems not a postfix notation")
+                return None
+            
+        elif walker == '/':
+            tmp = stk.pop()
+            if tmp == float(0):     # if the function tries divide by 0
+                print("division by 0 is impossible")
+                return None
+                
+            try:
+                stk.push(stk.pop() / tmp)
+            except:
+                print("your formula seems not a postfix notation")
+                return None
+        
+        elif walker.isnumeric():        # if number, push it
+            stk.push(float(walker))
+        
+        # if the string contains except the operands and operators,
+        else:
+            print("wrong data!")
+            return None
+        
+    return stk
+            
 
 if __name__ == "__main__":
     
-    p_notatioin = input("Type only the postfix notation : ")
+    #todo1 : how to check whether this is postfix or not
+    formula = input("Type only the postfix notation (please type space for distinguish) ")    # take the notation
+    stk = calculatePostfix(formula)
     
-    # while(not isPostfix(p_notatioin)):
-    
+    if stk:
+        stk.disp()
+    else:
+        print("something's wrong...")
