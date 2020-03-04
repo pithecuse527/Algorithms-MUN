a7d56cc27237da1806c0ee2328fe191da7214af1
EC2 Default User <ec2-user@ip-172-31-41-237.us-east-2.compute.internal>
2020-03-04 19:44:24 +0000

    Implement the Unsorted Priority Queue class

--- labs/P-QueueBase.py
+++ labs/P-QueueBase.py
@@ -36,3 +36,40 @@ class PriorityQueueBase:
         """Rearrange the elements in the data"""
         for p in range(self._data_len - 1):     # O(n)
             self._data[p], self._data[p+1] = self._data[p+1], self._data[p]
+
+class UnsortedPriorityQueue(PriorityQueueBase):
+    """A min-oriented priority queue implemented with an unsorted list"""
+        
+    def _find_min_index(self):        # nonpublic
+        """Return Position of item with minimun key"""
+        if self.is_empty():
+            print('Priority queue is empty')
+            return None
+            
+        small = self._data[0]
+        walk = 1
+        
+        while walk < self._data_len:          # O(n)
+            if self._data[walk] < self._data[small]:
+                small = walk
+        return small
+    
+    def add(self, key, value):
+        """Add a key-value pair (unsorted order)"""
+        self._data[self._data_len] = self._Item(key, value)
+        self._data_len += 1
+    
+    def min_(self):
+        """Return but do not remove (k,v) tuple with minimun key"""
+        item = self._data[self._find_min_index()]
+        return (item._key, item._value)
+    
+    def remove_min(self):
+        """Remove and return (k,v) tuple with minimun key"""
+        p = self._find_min_index()
+        item = self._data[p]
+        
+        self._rearrange(p)
+        self._data_len -= 1
+        
+        return (item._key, item._value)
