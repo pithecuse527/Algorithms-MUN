dba67c7b503f882e9c6943f921e2d062c88c8352
EC2 Default User <ec2-user@ip-172-31-41-237.us-east-2.compute.internal>
2020-03-05 15:27:25 +0000

    change the data structure to linked list

--- labs/P-QueueBase.py
+++ labs/P-QueueBase.py
@@ -14,70 +14,90 @@ class PriorityQueueBase:
     
     class _Item:
         """Lightweight composite to store priority queue items"""
-        __slots__ = '_key', '_value'
+        __slots__ = '_key', '_value', '_next'
         
-        def __init__(self, k, v):
+        def __init__(self, k, v, n=None):
             self._key = k
             self._value = v
+            self._next = n
         
+        def __lt__(self, other):
+            return self._key < other._key   # compare items based on their keys
+    
+        # getters
         def getKey(self):
             return self._key
         
         def getVal(self):
             return self._value
-            
-        def __lt__(self, other):
-            return self._key < other._key   # compare items based on their keys
-    
+        
+        def getNext(self):
+            return self._next
+        
+        def setNext(self, n):
+            self._next = n
+
     def __init__(self):
-        """Create a new empty Priority Queue"""
-        self._data = []
-        self.last_idx = -1
+        """Create a head of Priority Queue"""
+        self._head = None
+        self._tail = None
         
     def isEmpty(self):                 # concrete method assuming abstract len
         """Return True if the priority queue is empty"""
-        return len(self._data) == 0
-    
-    def _rearrange(self, p):
-        """Rearrange the elements in the data from p to last index and pop the last"""
-        for p in range(self.last_idx - 1):     # O(n)
-            self._data[p], self._data[p+1] = self._data[p+1], self._data[p]
-        self._data.pop()
+        return self._head is None
 
 
 class UnsortedPriorityQueue(PriorityQueueBase):
     """A min-oriented priority queue implemented with an unsorted list"""
-        
-    def _find_min_idx(self):        # nonpublic
-        """Return Position of item with minimun key"""
-        if self.isEmpty():
-            print('Priority queue is empty')
-            return None
-            
-        small = self._data[0]
-        walk = 1
-        
-        while walk < self.last_idx:          # O(n)
-            if self._data[walk] < self._data[small]:
-                small = walk
-        return small
     
-    def add(self, key, value):
+    def _findMin(self):         # nonpublic
+        """Return the item with minimum key"""
+        if self.isEmpty():
+            print("priority queue is empty")
+            return None
+        
+        small = self._head
+        walk = small.getNext()
+        
+        while walk:
+            if walk.getKey() < small.getKey():
+                small = walk
+            walk = walk.getNext()
+        
+        return small
+        
+    def add(self, k, v):
         """Add a key-value pair (unsorted order)"""
-        self._data[self.last_idx] = self._Item(key, value)
-        self.last_idx += 1
+        if self.isEmpty():
+            self._head = self._Item(k, v)
+            self._tail = self._head
+            return
+        
+        self._tail.setNext(self._Item(k, v))
+        self._tail = self._tail.getNext()
     
     def min_(self):
         """Return but do not remove (k,v) tuple with minimun key"""
-        item = self._data[self._find_min_index()]
-        return (item._key, item._value)
+        item = self._findMin()
+        return (item.getKey(), item.getVal())
     
     def remove_min(self):
         """Remove and return (k,v) tuple with minimun key"""
-        p = self._find_min_index()
-        item = self._data[p]
+        if self.isEmpty():
+            print("priority queue is empty")
+            return None
         
-        self._rearrange(p)
-        self.last_idx -= 1
+        item = self._findMin()
+        return_key = item._key
+        return_val = item._value
         
-        return (item._key, item._value)
+        walk = self._head
+        while True:
+            if walk.getNext() is item or walk is None:
+                break
+            walk = walk.getNext()
+        
+        walk.setNext(item.getNext())
+        item = None
+        
+        return (return_key, return_val)
